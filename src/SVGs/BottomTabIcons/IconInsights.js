da88591a27a1d2a8bf0765a5b7e7131fc7d7388c
Nikita Sawant <nikinsaw@gmail.com>
2026-08-11 17:24:09 +0530

    fix: fix icon issue

--- src/SVGs/BottomTabIcons/IconInsights.js
+++ src/SVGs/BottomTabIcons/IconInsights.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import Svg, { Path } from "react-native-svg"
-const SvgComponent = ({width, fill, ...rest}) => (
+const SvgComponent = ({width, height, fill, ...rest}) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
